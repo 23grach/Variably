2183c61d66b1abb1090b89be758b1e63e1d3dac1
Pavel Grachev <pasha.grach.g@gmail.com>
2025-06-26 17:41:58 +0300

    Update UI dimensions and implement a light theme color palette with improved styling for the Figma plugin

--- code.ts
+++ code.ts
@@ -7,7 +7,7 @@
 // full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__, { width: 340, height: 500 });
+figma.showUI(__html__, { width: 400, height: 700 });
 
 // Interface definitions
 interface VariableData {
