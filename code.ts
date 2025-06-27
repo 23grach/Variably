0e2cc1b67b86c7a7d228f55e59336c30c81db833
Pavel Grachev <pasha.grach.g@gmail.com>
2025-06-27 10:01:31 +0300

    Rename plugin from "Variables Sheet" to "Variably" and update UI comments for clarity in the Figma plugin.

--- code.ts
+++ code.ts
@@ -1,14 +1,4 @@
-/**
- * Variables Sheet Plugin for Figma
- * Автоматически создает таблицу со всеми переменными из выбранной коллекции
- */
 
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
-
-// This shows the HTML page in "ui.html".
 figma.showUI(__html__, { width: 400, height: 700 });
 
 /**
